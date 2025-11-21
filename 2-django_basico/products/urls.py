b8ec79d19fcc362d9547d276d127874ab7b11643
HenrickyL <Henrickyl1@gmail.com>
2025-11-21 01:06:24 -0300

    feat: setup static

--- 2-django_basico/products/urls.py
+++ 2-django_basico/products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.get_products, name="products"),
+    path("", views.insert_product, name="insert")
+    
 ]
--- 2-django_basico/products/views.py
+++ 2-django_basico/products/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return  HttpResponse("hello")
+def get_products(request):
+    return render(request, "products/index.html")
+
+
+def insert_product(request):
+    return HttpResponse("")
